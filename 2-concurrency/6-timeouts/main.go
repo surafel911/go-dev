3cf3379536decbf8a1d4e6f2cbc6ebf005d4456a
Surafel Assefa <surafel911@gmail.com>
2022-05-23 18:11:43 -0400

    Adds timeout concept to tutorial.

---
+++ 2-concurrency/6-timeouts/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/* Timouts are important when connecting to external resources in a
+ * time dependent program. This concept is implemented with the 
+ * `time.After()` method and a select statement case.
+ */
+func main() {
+	/* Creating the channel with a buffer of 1 prevents the anonymous
+	 * goroutine from hanging and allows it to close once finished.
+	 */
+	c1 := make(chan string, 1)
+
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "result 1"
+	}()
+
+	/* In this example, the message is recieved before the timeout occurs.
+	 * A result message should be printed.
+	 */
+	select {
+	case res := <- c1:
+		fmt.Println(res)
+	/* the `time.After()` method returns a channel with the actual elapsed
+	 * time. In this example, we only care that a message was recieved on
+	 * that channel, so we simply recieve from from the returned channel in
+	 * the case statement.
+	 */
+	case <- time.After(time.Second * 2):
+		fmt.Println("timeout 1")
+	}
+
+	c2 := make(chan string, 1)
+
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "result 2"
+	}()
+
+	/* In this example, the message is recieved after the timeout occurs.
+	 * A timout message should be printed.
+	 */
+	select {
+	case res := <- c2:
+		fmt.Println(res)
+	case <- time.After(time.Second * 1):
+		fmt.Println("timeout 2")
+	}
+}
