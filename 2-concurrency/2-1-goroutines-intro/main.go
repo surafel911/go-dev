1f69429d7bf54ac431ca2e8e87fbb5ff14c8ce3a
Surafel Assefa <surafel911@gmail.com>
2022-05-23 23:00:04 -0400

    Adds caveat that goroutines are not threads.

--- 2-concurrency/2-1-goroutines-intro/main.go
+++ 2-concurrency/2-1-goroutines-intro/main.go
@@ -8,12 +8,17 @@ import (
 func main() {
 	/* By adding the `go` keyword to a function call, it makes the function
 	 * run concurrently. This is called a `goroutine`. Even the main thread is
-	 * executed on a goroutine.
+	 * executed on a goroutine. A Goroutine is an independently executing
+	 * function with it's own stack.
 	 * 
 	 * Now this program has two goroutines: main and count("sheep"). It
 	 * should print sheeps and fishes. Goroutines are cheap to create/destroy
 	 * but realize they are limited by the hardware (i.e., # of logical
 	 * threads).
+	 *
+	 * Goroutines *are not threads*. Goroutines can be multiplexed into N>=1
+	 * threads running in a program. However, thinking goroutines are as
+	 * cheap as threads isn't far off.
 	 */
 	go count("sheep")
 	go count("fish")
